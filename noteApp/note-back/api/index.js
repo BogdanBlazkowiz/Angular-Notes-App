128481871d45d6dc5ca345f42e4168a96aa91fb0
Malherbe Matthieu <74493219+BogdanBlazkowiz@users.noreply.github.com>
2024-09-09 15:40:44 +0200

    Update index.js

--- noteApp/note-back/api/index.js
+++ noteApp/note-back/api/index.js
@@ -6,17 +6,16 @@ const port = process.env.PORT || 3000;
 const app = express();
 
 const cors = require("cors"); //
-app.use(cors({ origin: ["http://localhost:4200"] }));
+app.use(cors({ origin: ["https://angular-notes-app-1.onrender.com/"] }));
 
 app.use(express.urlencoded({ extended: false }));
-app.use(express.json())
-const url = "mongodb+srv://ForgotPassword:XHlQyfs4TTV9xyem@cluster0.zbtcm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+app.use(express.json());
+const url =
+    "mongodb+srv://ForgotPassword:XHlQyfs4TTV9xyem@cluster0.zbtcm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 mongoose.connect(url);
 
-
 app.get("/api/get-notes", async (req, res) => {
-    res.set('Access-Control-Allow-Origin', 'https://angular-notes-app-1.onrender.com/');
     res.json(await Note.find());
 });
 
@@ -26,7 +25,6 @@ app.post("/api/new-note", async (req, res) => {
             title: req.body.title,
             description: req.body.description,
         });
-	res.set('Access-Control-Allow-Origin', 'https://angular-notes-app-1.onrender.com/');
         res.json(newNote);
     } catch (err) {
         console.log(err);
@@ -35,13 +33,11 @@ app.post("/api/new-note", async (req, res) => {
 
 app.delete("/api/delete/:id", async (req, res) => {
     await Note.findByIdAndDelete(req.params.id);
-    res.set('Access-Control-Allow-Origin', 'https://angular-notes-app-1.onrender.com/');
     res.json("Deleted succesfully");
 });
 
 app.put("/api/update/:id", async (req, res) => {
     await Note.findByIdAndUpdate(req.params.id, req.body);
-    res.set('Access-Control-Allow-Origin', 'https://angular-notes-app-1.onrender.com/');
     res.json("Updated succesfully");
 });
 
